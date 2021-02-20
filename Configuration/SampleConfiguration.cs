fc7418afd9fb1f41a187a9c60355dd0475f0f309
Mathias Feitzinger <mathias.feitzinger@world-direct.at>
2021-02-20 20:46:22 +0100

    config samples

--- Configuration/SampleConfiguration.cs
+++ Configuration/SampleConfiguration.cs
@@ -1,9 +1,21 @@
 ﻿namespace TyrolSky.Portal.Configuration {
+    using System;
+    using System.Runtime.InteropServices;
+
     public sealed class SampleConfiguration {
 
         public static string ConfigPath = "Sample";
 
         public string SampleValue { get; set; }
 
+        public PersonInfo Person { get; set; }
+
+        public TimeSpan SampleTimeSpan { get; set; }
+
+    }
+
+    public class PersonInfo {
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
--- TyrolSky.Portal/Controllers/WeatherForecastController.cs
+++ TyrolSky.Portal/Controllers/WeatherForecastController.cs
@@ -1,30 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-
-namespace TyrolSky.Portal.Controllers {
+﻿namespace TyrolSky.Portal.Controllers {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using Configuration;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
 
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase {
-        private static readonly string[] Summaries = new[] {
+        public IOptions<SampleConfiguration> Configuration { get; }
+
+        private static readonly string[] Summaries = {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger) {
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IOptionsSnapshot<SampleConfiguration> configuration) {
+            Configuration = configuration;
             _logger = logger;
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get([Required] string sample) {
-            _logger.LogInformation("This is a test call");
-            var rng = new Random();
+            _logger.LogInformation("This is a test call {@ConfigValue}", Configuration.Value);
+            
+            Random rng = new Random();
             return Enumerable.Range(1, 5)
                 .Select(index => new WeatherForecast {
                     Date = DateTime.Now.AddDays(index),
